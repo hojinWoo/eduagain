a62a4492ba44e4adefc6497f510537d9bc922e49
hojinWoo <dnghwls7@ajou.ac.kr>
2018-08-16 16:49:50 +0900

    day01..

---
+++ 1. Java/day01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+  public static void main(String[] args){
+    System.out.println("Hello world");
+    System.out.println("my name is xxx");
+    System.out.println(100);
+  }
+}
---
+++ 1. Java/day01/Variable.java
@@ -0,0 +1,18 @@
+public class Variable{
+  public static void main(String[] args){
+    System.out.println("Variable Test");
+    //int a,b,c;
+
+    //assign
+    //a = 10;
+    //b = 20;
+    //c = 30;
+
+    //a = 10, b = 20, c = 30;
+
+    int a = 10, b = 20, c=30;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
