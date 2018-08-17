dc6a3a6a73023eb32568a158e8ee1ccbfbd7224d
hojinWoo <dnghwls7@ajou.ac.kr>
2018-08-17 17:57:52 +0900

    ing~~

--- 1. Java/day02/Gugudan.java
+++ 1. Java/day02/Gugudan.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Gugudan{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("input your number");
-    int num = sc.nextInt();
+  static void gugu(int num){
     for(int i = 1; i<10; i++){
-      for(int j=2;j<=num;j++){
+      for(int j = 2;j <= num; j++){
         System.out.print(j + " * " + i +" = " + i*j +"\t");
       }
       System.out.println();
     }
+  }
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("input your number");
+    int num = sc.nextInt();
+    gugu(num);
   }
 }
