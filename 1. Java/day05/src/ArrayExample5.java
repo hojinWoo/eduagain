7b3fd3a702ce14c819fc886137fa44d763b198e0
hojinWoo <dnghwls7@ajou.ac.kr>
2018-08-22 16:36:52 +0900

    ing~~

---
+++ 1. Java/day05/src/ArrayExample5.java
@@ -0,0 +1,17 @@
+/**
+ * Reference 배열 
+ * 다차원 배열
+ * @author hojin
+ *
+ */
+public class ArrayExample5 {
+
+	public static void main(String[] args) {
+		String[][] array = new String[3][10];
+		array[0][0] = "AAA";
+		array[2][0] = "ZZZ";
+		
+		String[][] array2 = new String[][] {{"a", "b", "c"}, {"d", "e", "f"},{"g","h","i"} };
+		String[][] array3 = {{"a", "b", "c"}, {"d", "e", "f"},{"g","h","i"} };
+	}
+}
---
+++ 1. Java/day05/src/FileReader.java
@@ -0,0 +1,18 @@
+/**
+ * java FileReader [읽고자 하는 파일] [읽고자 하는 파일명]
+ * @author hojin
+ *
+ */
+public class FileReader {
+	 public static void main(String[] args) {
+		 //java.lang.ArrayIndexOutOfBoundsException : 실행하면서 값을 저장하지 않았기 때문
+		if(args.length != 2) {
+			System.out.println("error");
+		}else {
+			String fileName1 = args[0];
+			String fileName2 = args[1];
+			System.out.println(fileName1);
+			System.out.println(fileName2);
+		}
+	}
+}
