eefa6643cc6e1ad75ff82b1ed2852ff244f620be
hojinWoo <dnghwls7@ajou.ac.kr>
2018-10-11 14:41:39 +0900

    servlet

---
+++ 3. HTML & CSS & JS/day36 Servlet/src/kr/or/kosta/servlet/MIMEServlet2.java
@@ -0,0 +1,43 @@
+package kr.or.kosta.servlet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MIMEServlet
+ */
+public class MIMEServlet2 extends HttpServlet {
+	private String fileRepository = "C:\\Users\\KOSTA\\Desktop\\eclipse workspace\\ServletStudy\\web\\assets\\";
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String requestFile = "music.mp3";
+	     
+	     // 응답헤더에 Content-Type 설정
+	     response.setContentType("audio/mpeg");
+	     
+	     // 바이트입력스트림 생성
+	     InputStream in = new FileInputStream(fileRepository + requestFile);
+	     // response가 제공하는 바이트입력스트림 취득
+	     OutputStream out = response.getOutputStream();
+	     byte[] buffer = new byte[1024];
+	     int count = 0;
+	     try{
+	          while( (count = in.read(buffer)) != -1){
+	               out.write(buffer, 0, count);
+	          }
+	     }finally{
+	          if(out != null) out.close();
+	          if(in != null) in.close();
+	     }
+	}
+}
