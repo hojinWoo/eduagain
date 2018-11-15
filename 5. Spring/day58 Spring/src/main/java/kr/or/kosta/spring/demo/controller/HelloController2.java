904d610ee2b75ca0b98467d63dc1082f84a8f26f
hojinWoo <dnghwls7@ajou.ac.kr>
2018-11-15 17:50:32 +0900

    RequestMapping ~~

--- 5. Spring/day58 Spring/src/main/java/kr/or/kosta/spring/demo/controller/HelloController2.java
+++ 5. Spring/day58 Spring/src/main/java/kr/or/kosta/spring/demo/controller/HelloController2.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 2.5 버전 이후 사용 예제 (P	OJO)
@@ -26,6 +27,22 @@ public class HelloController2{
 		String time = String.format("%1$tF %1$tT", calendar);
 		model.addAttribute("today", time);
 		return "demo/today";
+	}
+	
+//	/find
+	@RequestMapping(value="/find")
+	public String find(Model model){
+		String message = "일반 page입니다.";
+		model.addAttribute("message", message);
+		return "demo/admin";
+	}
+
+//	/find?admin=true
+	@RequestMapping(value="/find", method=RequestMethod.GET, params="admin=true")
+	public String findAdmin(Model model){
+		String message = "admin page입니다.";
+		model.addAttribute("message", message);
+		return "demo/admin";
 	}
 
 }
